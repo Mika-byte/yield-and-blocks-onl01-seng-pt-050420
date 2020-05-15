caee707b1b379a7cd5338e4062c54894b7163ed1
Michaelle Barbor <michaelle.barbor@outlook.com>
2020-05-15 01:12:43 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,4 @@
 
- 
  def hello_t(array)
   i = 0
   
@@ -7,13 +6,13 @@
   yield array [i]
   i = i + 1
   end
+  array
  end 
  
  # call your method here!
 
- 
- hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
+# hello_t(["Tim", "Tom", "Jim"]) do |name|
+#   if name.start_with?("T")
+#     puts "Hi, #{name}"
+#   end
+# end
