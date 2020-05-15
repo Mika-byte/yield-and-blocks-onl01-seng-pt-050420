5100853a94d05343f51c63c1803a2b0600278183
Michaelle Barbor <michaelle.barbor@outlook.com>
2020-05-15 01:10:30 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,14 +1,5 @@
-# names = ["Tim", "Tom", "Jim"]
-# def hello_t(array)
-# names.each do |name|
-#   puts  "#{name}"
-#   end
-# end
 
-
-# other_names = []
-# def yielding(array)
-# # call your method here!
+ # call your method here!
 
 def hello_t(array)
   i = 0
